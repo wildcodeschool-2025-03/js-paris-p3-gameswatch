1b7475a784618e9388a0eb30d2b44590c1c6f5ab
Hamidou <hake92240@gmail.com>
2025-06-25 12:01:29 +0200

    sitting

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,13 +1,12 @@
 import "./Profile.css";
-import Footer from "../components/Footer";
-import Nav from "../components/Nav";
 
 function Profile() {
   return (
     <>
-      <Nav />
-      <h1>Hellow word</h1>
-      <Footer />
+
+      <div className='carré'>
+
+      </div>
     </>
   );
 }
